f6248ddf05c3e25ecc972bad570160f11748c7c7
Guilherme Casagrande <thegui4000@gmail.com>
2022-07-28 12:22:05 -0300

    fazendo o agendamento online

--- atividade3/arq/default.php
+++ atividade3/arq/default.php
@@ -1,1 +1,3 @@
-<h1>Escolha uma das opções ao lado para agendar uma reuinão</h1>
+<div class="default">
+    <h1>Escolha uma das opções ao lado para agendar uma reuinão</h1>
+</div>
--- atividade3/arq/online.php
+++ atividade3/arq/online.php
@@ -1,1 +1,8 @@
-<h1>Teste</h1>
+<div class="online">
+    <h1>Agende Uma reunião Online</h1>
+    <form action="../index.php" method="post">
+        <input type="text" name="nome" id="nome" placeholder="Digite seu nome aqui">
+
+
+    </form>
+</div>
